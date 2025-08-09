f22910a90f035849c3839a2aa711ab7ff604c688
Emanuel <105359101+emanuelhmarcineksilva@users.noreply.github.com>
2025-08-09 10:13:56 -0300

    Add files via upload

---
+++ numero.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Sucessor e antecessor/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+    <main> <!-- Quando vc escreve main aparece dois e para o php devese clicar no que tem uma chave de fenda ao lado -->
+        <?php 
+        $num = $_POST['n'] ?? 0;
+
+        echo "<p>Analizando o número <b>" . number_format($num, 3, ",", ".") . "</b>:</p>";
+
+        $int = (int) $num; // Esse (int) pega o número inteiro
+        $fra = $num - $int;
+
+        echo "<ul><li> A inteira é <b>" . number_format($int, 0, ",", ".") ."</b></li>";
+        echo "<li> A fracionada é <b>" . number_format($fra, 3, ",", ".") ."</b></li></ul>";
+
+        ?>
+        <button onclick="javascript:history.go(-1)" class="voltar">Voltar</button>
+    </main></section>
+</body>
+</html>
