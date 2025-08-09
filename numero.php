c06b655f08f1272975940c781683f7894886fa8e
Emanuel <105359101+emanuelhmarcineksilva@users.noreply.github.com>
2025-08-09 11:10:45 -0300

    Update numero.php
    Arrumando o Style.css

--- numero.php
+++ numero.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Sucessor e antecessor/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -24,4 +24,5 @@
         <button onclick="javascript:history.go(-1)" class="voltar">Voltar</button>
     </main></section>
 </body>
+
 </html>
